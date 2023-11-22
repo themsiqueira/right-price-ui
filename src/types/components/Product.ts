5ff479b68696964a3905735555ed6edaa64a7710
matheuxito <matheus.souza28042001@gmail.com>
2023-11-21 21:53:21 -0300

    feat: add Product interface

---
+++ src/types/components/Product.ts
@@ -0,0 +1,12 @@
+interface Product {
+  id: number,
+  name: string;
+  price: string;
+  newPrice: string;
+  category: string;
+  market: string;
+  imageUrl: string;
+  registerDate: string;
+}
+
+export type { Product };
